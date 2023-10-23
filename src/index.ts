26ec1bdd62621914d67c0f2cfaef83b50139f98b
lumusitech <lumusitech@gmail.com>
2023-10-23 14:04:53 -0300

    feat: add simple use of observer and observable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Observable } from "rxjs";
+import { Observable, Observer } from "rxjs";
+
+const observer: Observer<string> = {
+  next: (next) => console.log("next", next),
+  error: (error) => console.warn("error: ", error),
+  complete: () => console.info("complete"),
+};
 
 const obs$ = new Observable<string>((subs) => {
   subs.next("Hola");
@@ -6,8 +12,18 @@ const obs$ = new Observable<string>((subs) => {
   subs.next("Hola");
   subs.next("Mundo");
 
+  // Forzar un error
+  //   const a = undefined;
+  //   a.nombre = "Luciano";
+
   subs.complete();
   subs.next("este valor ya no lo ven");
 });
 
-obs$.subscribe(console.log);
+// obs$.subscribe({
+//   next: (next) => console.log("next", next),
+//   error: (error) => console.warn("error: ", error),
+//   complete: () => console.log("complete"),
+// });
+
+obs$.subscribe(observer);
