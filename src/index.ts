c754a0d235e14fc90606260638904877c2c0ad4a
lumusitech <lumusitech@gmail.com>
2023-10-23 13:43:51 -0300

    feat: add first observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import { Observable } from "rxjs";
 
+const obs$ = new Observable<string>((subs) => {
+  subs.next("Hola");
+  subs.next("Mundo");
+  subs.next("Hola");
+  subs.next("Mundo");
 
-console.log('Hola Mundo!');
+  subs.complete();
+  subs.next("este valor ya no lo ven");
+});
 
-
-
-
-
-
-
+obs$.subscribe(console.log);
